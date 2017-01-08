5fb9e8225f75ddc496a5d59c20f35867fb97c890
Shreya4net <pujakumari.civil@gmail.com>
2017-01-08 18:10:57 +0530

    java session7 assignment1
    java session7 assignment1

---
+++ Stringass1.java
@@ -0,0 +1,11 @@
+package session7;
+
+public class Stringass1 {
+public static void main(String[] args){
+String w="RaM";
+
+
+System.out.println(w.length());
+System.out.println(w.charAt(2));
+}
+}
